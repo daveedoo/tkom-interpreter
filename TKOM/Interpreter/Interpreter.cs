d3716cfe26e898e4f43d8a095ab1766de9719521
daveedoo <73546387+daveedoo@users.noreply.github.com>
2021-12-27 14:39:44 +0100

    Preliminary sketch of interpreter classes

---
+++ TKOM/Interpreter/Interpreter.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+
+namespace TKOM.Interpreter
+{
+    //internal class SymbolsTable
+    //{
+    //    public HashSet<ISymbol> Symbols { get; }
+    //}
+
+    //internal interface ISymbol
+    //{
+    //    //public Type Type { get; }
+    //    public string Name { get; }
+    //}
+
+    //internal class IntVariable : ISymbol
+    //{
+    //    public string Name { get; }
+    //    public int? Value { get; }
+    //}
+
+    //internal class Function : ISymbol
+    //{
+    //    public Type Type { get; }
+    //    public string Name { get; }
+    //    public IList<Parameter> Parameters { get; }
+    //}
+
+    internal class Variable
+    {
+        public string Name { get; }
+        public IValue Value { get; }
+
+        public Variable(string name, IValue value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+
+    internal interface IValue
+    { }
+
+    internal class IntegerValue : IValue
+    {
+        public int Value { get; }
+
+        public IntegerValue(int value)
+        {
+            Value = value;
+        }
+    }
+
+    internal class Scope
+    {
+        public List<Variable> Variables { get; }
+
+        public Scope()
+        {
+            Variables = new List<Variable>();
+        }
+    }
+
+    internal class FunctionCallContext
+    {
+        public Stack<Scope> Scopes { get; }
+
+        public FunctionCallContext()
+        {
+            Scopes = new Stack<Scope>();
+        }
+    }
+
+    internal class Interpreter
+    {
+        public Stack<FunctionCallContext> CallStack { get; }
+
+        public Interpreter()
+        {
+            CallStack = new Stack<FunctionCallContext>();
+
+            IntegerValue zero = new IntegerValue(0);
+            Variable testVar = new Variable("x", zero);
+        }
+    }
+}
