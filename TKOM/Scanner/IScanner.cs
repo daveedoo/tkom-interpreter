3233d6ffc928fabb1a3b0959e320503c79b0dfe3
daveedoo <73546387+daveedoo@users.noreply.github.com>
2022-01-24 01:59:22 +0100

    Some documentation for Scanner

--- TKOM/Scanner/IScanner.cs
+++ TKOM/Scanner/IScanner.cs
@@ -5,10 +5,24 @@ namespace TKOM.Scanner
 {
     public interface IScanner : IEnumerator<Token>
     {
+        /// <summary>
+        /// <c>string</c> representation of the current Token - if the Token has one.
+        /// Null otherwise.
+        /// </summary>
         public string StringValue { get; }
+        /// <summary>
+        /// <c>string</c> representation of the current Token - if the Token has one.
+        /// Null otherwise.
+        /// </summary>
         public int? IntValue { get; }
 
+        /// <summary>
+        /// Current position in the input stream.
+        /// </summary>
         public Position Position { get; }
+        /// <summary>
+        /// Object used for handling errors occurred in the process of scanning the input.
+        /// </summary>
         public IErrorHandler ErrorHandler { get; }
     }
 }
